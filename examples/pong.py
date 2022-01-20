3a4083a96eb5802c5ab29b19b88600a28490732b
Rui Hirano <r.hrn.0930@gmail.com>
2022-01-21 00:10:56 +0900

    add pong example

---
+++ examples/pong.py
@@ -0,0 +1,17 @@
+import gym
+import dreamerv2.lib as dv2
+
+config = dv2.defaults.update({
+    'logdir': '~/logdir/pong',
+    'log_every': 1e3,
+    'train_every': 10,
+    'prefill': 1e5,
+    'actor_ent': 3e-3,
+    'loss_scales.kl': 1.0,
+    'discount': 0.99,
+    'save_every': 1e5
+}).parse_flags()
+import gym
+env = gym.make('Pong-v0')
+dreamerv2 = dv2.DreamerV2(env, config)
+dreamerv2.train()
